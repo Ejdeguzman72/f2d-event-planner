d94d66010890a79c51bb2b6dcfe85ec0cb5d880b
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-09-16 00:08:33 -0400

    more changes

--- src/main/java/com/f2d/event_planner/EventPlannerApplication.java
+++ src/main/java/com/f2d/event_planner/EventPlannerApplication.java
@@ -2,8 +2,10 @@ package com.f2d.event_planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventPlannerApplication {
 
 	public static void main(String[] args) {
