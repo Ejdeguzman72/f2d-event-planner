690047455cc07b525e2950ab4815381ea8a9f990
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-11-07 21:09:42 -0500

    more changes

--- src/main/java/com/f2d/event_planner/controller/EventController.java
+++ src/main/java/com/f2d/event_planner/controller/EventController.java
@@ -8,32 +8,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class EventController {
 
     @Autowired
     private EventService eventService;
 
     @GetMapping(value = UriConstants.RETRIEVE_ALL_EVENTS_URI)
+    @CrossOrigin(origins = "*")
     public EventListResponse retrieveAllEventInformation() {
         return eventService.retrieveAllEventInfo();
     }
 
     @GetMapping(value = UriConstants.RETRIEVE_EVENT_BY_ID_URI)
+    @CrossOrigin(origins = "*")
     public EventSearchResponse retrieveEventById(@PathVariable UUID eventId) {
         return eventService.retrieveEventById(eventId);
     }
 
     @PostMapping(value = UriConstants.CREATE_EVENT_URI)
+    @CrossOrigin(origins = "*")
     public EventAddUpdateResponse createEvent(@RequestBody EventAddUpdateRequest request) {
         return eventService.createEvent(request);
     }
 
     @PutMapping(value = UriConstants.UPDATE_EVENT_URI)
+    @CrossOrigin(origins = "*")
     public EventAddUpdateResponse updateEvent(@RequestBody EventAddUpdateRequest request, @PathVariable UUID eventId) {
         return eventService.updateEvent(request,eventId);
     }
 
     @DeleteMapping(value = UriConstants.DELETE_EVENT_URI)
+    @CrossOrigin(origins = "*")
     public EventSearchResponse deleteEvent(@PathVariable UUID eventId) {
         return eventService.deleteEventInfo(eventId);
     }
