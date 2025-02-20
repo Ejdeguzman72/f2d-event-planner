bb23a2360db1cd54a355e5da08b56e400744faa0
Ejdeguzman72 <ejdeguzman72@gmail.com>
2025-02-19 19:20:32 -0500

    more changes

--- f2d-event-planner/src/main/java/com/f2d/event_planner/config/FeignConfig.java
+++
@@ -1,9 +0,0 @@
-package com.f2d.event_planner.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "com.f2d.event_planner.feign")
-public class FeignConfig {
-}
